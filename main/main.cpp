e5846e98b5200fccc068f2d1e919cac641862c2a
hongquan.li <hongquan.prog@gmail.com>
2024-08-22 11:38:33 +0800

    add feature to read memory

--- main/main.cpp
+++ main/main.cpp
@@ -11,7 +11,7 @@
 #include <signal.h>
 #include <iostream>
 #include <fstream>
-#include <filesystem> 
+#include <filesystem>
 #include "log.h"
 #include "flash_iface.h"
 #include "file_programmer.h"
@@ -142,6 +142,44 @@ bool file_exist(const std::string &path)
     return file.is_open();
 }
 
+bool read_memory(std::shared_ptr<SWDIface> swd, uint32_t addr, uint32_t length)
+{
+    uint32_t rd_size = 0;
+    uint32_t rd_begin = addr;
+    uint32_t rd_end = addr + length;
+    std::shared_ptr<uint8_t[]> buf = std::shared_ptr<uint8_t[]>(new uint8_t[32]);
+
+    if (swd->set_target_state(SWDIface::TARGET_HALT) != true)
+    {
+        return false;
+    }
+
+    memset(buf.get(), 0, 32);
+    std::cout << "memory content:" << std::endl;
+    for (uint32_t i = rd_begin; i < rd_end; i += rd_size)
+    {
+        rd_size = rd_end - i;
+        rd_size = (rd_size > 32) ? (32) : (rd_size);
+        if (swd->read_memory(i, buf.get(), rd_size) != true)
+        {
+            swd->set_target_state(SWDIface::TARGET_RUN);
+            return false;
+        }
+
+        std::cout << std::hex << std::setw(8) << std::setfill('0') << i << ": ";
+        for (uint32_t j = 0; j < rd_size; ++j)
+        {
+            std::cout << std::setw(2) << std::setfill('0') << static_cast<unsigned int>(buf[j]) << " ";
+        }
+
+        std::cout << "\n";
+    }
+
+    swd->set_target_state(SWDIface::TARGET_RUN);
+
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
     std::shared_ptr<SWDIface> swd;
@@ -154,56 +192,89 @@ int main(int argc, char *argv[])
     uint32_t swclk_pin = 0;
     uint32_t swdio_pin = 0;
     FlashIface::target_cfg_t cfg;
+    std::string command;
+    std::string algorithm;
+    std::string image;
+    uint32_t rd_addr;
+    uint32_t rd_size;
 
-    if (argc < 5 || argc > 6)
+    if (argc < 3)
     {
-        LOG_ERROR("Usage: %s swclk swdio algorithm image <addr>", argv[0]);
+        LOG_ERROR("Usage: %s <command> [options]", argv[0]);
+        LOG_ERROR("Commands: flash, read");
         return -1;
     }
-    else if (argc == 6)
+
+    command = argv[1];
+    if (command == "flash")
     {
-        base_addr = static_cast<uint32_t>(std::stoul(argv[5], nullptr, 16));
+        if (argc < 6 || argc > 7)
+        {
+            LOG_ERROR("Usage: %s flash swclk swdio algorithm image <addr>", argv[0]);
+            return -1;
+        }
+        else if (argc == 7)
+        {
+            base_addr = static_cast<uint32_t>(std::stoul(argv[6], nullptr, 16));
+        }
+
+        swclk_pin = static_cast<uint32_t>(std::stoul(argv[2], nullptr, 10));
+        swdio_pin = static_cast<uint32_t>(std::stoul(argv[3], nullptr, 10));
+        algorithm = argv[4];
+        image = argv[5];
+
+        try
+        {
+            swd = swd_create(swclk_pin, swdio_pin);
+            bin = std::make_shared<BinaryProgram>(*swd);
+            hex = std::make_shared<HexProgram>(*swd);
+            prog = std::make_shared<FileProgrammer>(*bin, *hex);
+            prog->register_progress_changed_callback(progress_handler);
+
+            // Finding Algorithms from Programs
+            if (Algorithm::find(algorithm_bin_ptr(), algorithm_bin_len(), algorithm, cfg))
+            {
+                gettimeofday(&start, NULL);
+                if (prog->program(image, cfg, base_addr))
+                {
+                    gettimeofday(&end, NULL);
+                    LOG_INFO("elapsed time: %ld ms", ((end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec)) / 1000);
+                }
+            }
+            // Finding Algorithms from file
+            else if (file_exist(algorithm))
+            {
+                gettimeofday(&start, NULL);
+                if (prog->program(image, algorithm, base_addr))
+                {
+                    gettimeofday(&end, NULL);
+                    LOG_INFO("elapsed time: %ld ms", ((end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec)) / 1000);
+                }
+            }
+            else
+            {
+                LOG_ERROR("Unknown device %s", algorithm.c_str());
+                Algorithm::list_device(algorithm_bin_ptr(), algorithm_bin_len());
+            }
+        }
+        catch (std::exception &e)
+        {
+            LOG_ERROR("%s", e.what());
+        }
     }
-
-    swclk_pin = static_cast<uint32_t>(std::stoul(argv[1], nullptr, 10));
-    swdio_pin = static_cast<uint32_t>(std::stoul(argv[2], nullptr, 10));
-
-    try
+    else if (command == "read")
     {
+        if (argc != 6)
+        {
+            LOG_ERROR("Usage: %s read swclk swdio addr length", argv[0]);
+            return -1;
+        }
+
+        swclk_pin = static_cast<uint32_t>(std::stoul(argv[2], nullptr, 10));
+        swdio_pin = static_cast<uint32_t>(std::stoul(argv[3], nullptr, 10));
+        rd_addr = static_cast<uint32_t>(std::stoul(argv[4], nullptr, 16));
+        rd_size = static_cast<uint32_t>(std::stoul(argv[5], nullptr, 16));
         swd = swd_create(swclk_pin, swdio_pin);
-        bin = std::make_shared<BinaryProgram>(*swd);
-        hex = std::make_shared<HexProgram>(*swd);
-        prog = std::make_shared<FileProgrammer>(*bin, *hex);
-        prog->register_progress_changed_callback(progress_handler);
-
-        // Finding Algorithms from Programs
-        if (Algorithm::find(algorithm_bin_ptr(), algorithm_bin_len(), argv[3], cfg))
-        {
-            gettimeofday(&start, NULL);
-            if (prog->program(argv[4], cfg, base_addr))
-            {
-                gettimeofday(&end, NULL);
-                LOG_INFO("elapsed time: %ld ms", ((end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec)) / 1000);
-            }
-        }
-        // Finding Algorithms from file
-        else if (file_exist(argv[3]))
-        {
-            gettimeofday(&start, NULL);
-            if (prog->program(argv[4], argv[3], base_addr))
-            {
-                gettimeofday(&end, NULL);
-                LOG_INFO("elapsed time: %ld ms", ((end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec)) / 1000);
-            }
-        }
-        else
-        {
-            LOG_ERROR("Unknown device %s", argv[3]);
-            Algorithm::list_device(algorithm_bin_ptr(), algorithm_bin_len());
-        }
-    }
-    catch (std::exception &e)
-    {
-        LOG_ERROR("%s", e.what());
+        read_memory(swd, rd_addr, rd_size);
     }
 }
